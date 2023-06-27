5bda6097513b4356a9c8f2972f7def62fc317efc
Asma <asmarana>
2023-06-27 12:24:48 +0500

    Quiz

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 	"quiz/csv"
 )
 
@@ -10,14 +12,21 @@ func main() {
 	question, answers := csv.ReadFile()
 	trueAns := 0
 	falseAns := 0
+	reader := bufio.NewReader(os.Stdin)
 	for i := range question {
 		fmt.Printf("========Question%d======= \n%v\n", i+1, question[i])
-		fmt.Scanf("Enter answer : ", ans[i])
-		if ans[i] == answers[i] {
+		fmt.Println("Enter your answer : ")
+		ans[i], _ = reader.ReadString('\n')
+		if answers[i] != ans[i] {
 			trueAns++
 		} else {
+			fmt.Printf("Correct Ans : %v but your answer is : %v", answers[i], ans[i])
 			falseAns++
 		}
+	}
+
+	for j := range ans {
+		fmt.Println(ans[j])
 	}
 
 	fmt.Printf("Correct Answers Are : %d\n", trueAns)
