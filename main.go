bdb70ab45a06a6069ba6ff327ca3f9d32f70a007
Asma <asmarana>
2023-06-27 15:57:18 +0500

    Quiz

--- main.go
+++ main.go
@@ -5,30 +5,42 @@ import (
 	"fmt"
 	"os"
 	"quiz/csv"
+	"sync"
 )
 
 func main() {
-	var ans [10]string
-	question, answers := csv.ReadFile()
-	trueAns := 0
-	falseAns := 0
-	reader := bufio.NewReader(os.Stdin)
-	for i := range question {
-		fmt.Printf("========Question%d======= \n%v\n", i+1, question[i])
-		fmt.Println("Enter your answer : ")
-		ans[i], _ = reader.ReadString('\n')
-		if answers[i] != ans[i] {
-			trueAns++
-		} else {
-			fmt.Printf("Correct Ans : %v but your answer is : %v", answers[i], ans[i])
-			falseAns++
+
+	ch := make(chan string, 10)
+	ch1 := make(chan string, 10)
+	wg := &sync.WaitGroup{}
+
+	wg.Add(1)
+
+	go func(ch chan string, ch1 chan string, wg *sync.WaitGroup) {
+		var ans [10]string
+		question, answers := csv.ReadFile()
+		trueAns := 0
+		falseAns := 0
+		reader := bufio.NewReader(os.Stdin)
+		for i := range question {
+			fmt.Printf("========Question%d======= \n%v\n", i+1, question[i])
+			fmt.Println("Enter your answer : ")
+			ans[i], _ = reader.ReadString('\n')
+			ch <- answers[i]
+			ch1 <- ans[i]
+			if ch == ch1 {
+				trueAns++
+			} else {
+				fmt.Printf("Correct Ans : %v but your answer is : %v", answers[i], ans[i])
+				falseAns++
+			}
 		}
-	}
 
-	for j := range ans {
-		fmt.Println(ans[j])
-	}
+		fmt.Println(ans)
 
-	fmt.Printf("Correct Answers Are : %d\n", trueAns)
-	fmt.Printf("False Answers Are : %d\n", falseAns)
+		fmt.Printf("Correct Answers Are : %d\n", trueAns)
+		fmt.Printf("False Answers Are : %d\n", falseAns)
+		wg.Done()
+	}(ch, ch1, wg)
+	wg.Wait()
 }
